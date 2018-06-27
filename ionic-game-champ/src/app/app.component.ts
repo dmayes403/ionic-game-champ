69efc6f4421ee3dc6ba3149f5e0ab8111d0667b3
David Mayes <davidmayes@Davids-MacBook-Pro.local>
2018-06-26 21:24:55 -0600

    adding beginning pages

--- ionic-game-champ/src/app/app.component.ts
+++ ionic-game-champ/src/app/app.component.ts
@@ -4,7 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { GamesPage } from '../pages/games/games';
+import { PlayersPage } from '../pages/players/players';
+import { StatsPage } from '../pages/stats/stats';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Games', component: GamesPage},
+      { title: 'Players', component: PlayersPage},
+      { title: 'Stats', component: StatsPage}
     ];
 
   }
--- ionic-game-champ/src/app/app.module.ts
+++ ionic-game-champ/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GamesPage } from '../pages/games/games';
+import { PlayerPage } from '../pages/player/player';
+import { StatsPage } from '../pages/stats/stats';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    GamesPage,
+    PlayerPage,
+    StatsPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    GamesPage,
+    PlayerPage,
+    StatsPage
   ],
   providers: [
     StatusBar,
